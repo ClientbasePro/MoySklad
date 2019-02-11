566d5bb516da3ca01f83e2cd21cf79e416986551
ClientbasePro <42553531+ClientbasePro@users.noreply.github.com>
2019-02-11 18:31:04 +0300

    Добавлены начальные файлы интеграции

---
+++ MoySkladFunctions.php
@@ -0,0 +1,23 @@
+<?php
+
+  // Интеграция CRM Clientbase и МойСклад (MoySklad)
+  // https://ClientbasePro.ru
+  // https://online.moysklad.ru/api/posap/1.0/doc/index.html
+  
+require_once 'common.php';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
